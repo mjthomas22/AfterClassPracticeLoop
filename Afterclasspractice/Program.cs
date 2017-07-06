399ef940a378e0ac3d9f1ce8879adc8aa86843d7
mjthomas22 <mthomas22@gmail.com>
2017-07-06 16:12:19 -0400

    first commit

---
+++ Afterclasspractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Afterclasspractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write Hello World ten times
+            for (int i = 0; i <10; i++) 
+            {
+                Console.WriteLine("Hello World");
+            }
+
+
+
+        }
+    }
+}
